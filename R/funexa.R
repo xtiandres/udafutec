c22815eaf8055b0e04c990f2bfe44086a227f8e5
xtiandres <xtiancuencap@gmail.com>
2022-03-31 23:00:28 -0500

    script practica funciones

---
+++ R/funexa.R
@@ -0,0 +1,60 @@
+# FUNCIONES - PRACTICA
+
+# Función: cuadrados de una sequencia de números
+n.f <- function(a) {
+  for(i in 1:a) {
+    b <- i^2
+    print(b)
+  }
+}
+ 
+n.f(13) 
+
+# Función: crear sin argumentos.
+n.f1 <- function() {
+  for(i in 1:5) {
+    print(i^2)
+  }
+}
+
+n.f1()
+
+# Función: crear con argumentos.
+n.f2 <- function(a,b,c) {
+  result <- a*b+c
+  print(result)
+}
+
+n.f2(2,4,3)
+
+# Función: crear con argumentos - cont.
+n.f3 <- function(a = 3,b =6) {
+  result <- a*b
+  print(result)
+}
+
+n.f3()
+n.f3(4, 5)
+
+# Función: ROC creada para medir "sensibilidad" y "especifidad"
+# de una variable continua 
+
+# Env para ROC
+x<-rnorm(100,mean=0)
+y<-rnorm(100, mean=1)
+isx<-rep(c(TRUE,FALSE),each=100)
+
+# Función ROC
+ROC <- function(test, disease){
+  cutpoints <- c(-Inf, sort(unique(test)), Inf)
+  sensitivity<-sapply(cutpoints,
+                      function(result) mean(test>result & disease)/mean(disease))
+  specificity<-sapply(cutpoints,
+                      function(result) mean(test<=result & !disease)/mean(!disease))
+  plot(sensitivity, 1-specificity, type="l")
+  abline(0,1,lty=2)
+  return(list(sens=sensitivity, spec=specificity))
+}
+
+ROC(c(x, y), isx)
+  
